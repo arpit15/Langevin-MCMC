8fbf69e59c721eb3ed0425a24662e9f3b1ee72c4
arpit agarwal <arpit15945@gmail.com>
2021-08-02 21:24:33 -0400

    Testing script

---
+++ scripts/test_scenes.py
@@ -0,0 +1,13 @@
+from os.path import join, basename, dirname, realpath
+from glob import glob
+import subprocess
+from ipdb import set_trace
+
+curr_dir = dirname(realpath(__file__))
+folder = join(curr_dir, "..", "scenes", "simple_test_scenes")
+
+fname_list = glob(join(folder, "*.xml"))
+
+for fname in fname_list:
+    print(f"Rendering {basename(fname)}")
+    subprocess.run([join(curr_dir, "../build-Release/dpt"), fname])
