caab8d8af65735a7b9e3d8bf7dc07f80fcce5d6c
arpit agarwal <arpit15945@gmail.com>
2021-08-03 14:13:17 -0400

    Validated scenes after iesarea light

--- scripts/test_scenes.py
+++ scripts/test_scenes.py
@@ -8,6 +8,7 @@ folder = join(curr_dir, "..", "scenes", "simple_test_scenes")
 
 fname_list = glob(join(folder, "*.xml"))
 
+# set_trace()
 for fname in fname_list:
     print(f"Rendering {basename(fname)}")
     subprocess.run([join(curr_dir, "../build-Release/dpt"), fname])
