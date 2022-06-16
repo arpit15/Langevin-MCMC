72616ba4a12a1f4937cd26528daa67f1a3cbda3d
Arpit Agarwal <arpit15945@gmail.com>
2022-06-16 18:56:10 +0000

    copy .so for pybuild

--- remote_build.sh
+++ remote_build.sh
@@ -12,3 +12,5 @@ export LMCEXE=/home/ubuntu/projects/lmc/build-Release/dpt
 
 cd ~/projects/lmc
 pip install -ve .
+# HACK
+cp _skbuild/linux-x86_64-3.8/cmake-build/python_bind/dpt_ext.cpython-38-x86_64-linux-gnu.so python_bind/lmc
